a97878a5a33120713bddbe50f0f8224cdcead18f
Ian Chen <ichen@osrfoundation.org>
2015-07-31 16:33:50 -0700

    add chapter desktop executable

---
+++ scripts/run.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+$DIR/stop.bash
+
+
+#./stop.bash
+
+echo "Did you source?:  <your setup path>/share/simplegdk/setup.sh"
+echo "Did you source?:  <your setup path>/share/gazebo/setup.sh"
+
+# get simplegdk configuration.
+if [ -f ${HOME}/local/share/simplegdk/setup.sh ]; then
+  . ${HOME}/local/share/simplegdk/setup.sh
+elif [ -f /usr/local/share/simplegdk/setup.sh ]; then
+  . /usr/local/share/simplegdk/setup.sh
+else
+  echo "Warning: failed to find simplegdk's setup.sh.  You will need to source it manually."
+fi
+
+
+echo "GAZEBO PLUGIN PATH=$GAZEBO_PLUGIN_PATH"
+echo "MENTOR2 world [$1]"
+
+#gzserver --verbose  -s libSimpleConnectionsPlugin.so $1 -u &
+#gzclient --verbose -g libCMLEditorPlugin.so  &
+
+
+echo "GAZEBO PREFIX=$GAZEBO_PREFIX"
+
+gzserver --verbose -s libRestWebPlugin.so -u  $1 &
+
+gzclient --verbose -g libCMLEditorPlugin.so -g libRestUiPlugin.so menu="Mentor2" title="Leaderboard" label="Login" url="https://localhost:3000" &
+
+
+sleep 10
